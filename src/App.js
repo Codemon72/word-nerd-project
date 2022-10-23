2cb4c74889c6ce861c23172e718490ed53b8905f
Clemens Bruesch <clemensbruesch@gmail.com>
2022-10-23 02:38:39 +0200

    fetch to API and log response

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+
+const API_URL = "https://api.datamuse.com/words?rel_syn=example";
+
+const fetchWords = async () => {
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) { // errors from server
+      throw Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log(data)
+    
+  } catch (error) { // errors from network / connection
+    console.log(error.message);
+  }
+};
+
+fetchWords();
+
+
 function App() {
   return (
     <div>
