f257c9e7b8f6cb1bd68c98116105d94b7fe09591
Clemens Bruesch <clemensbruesch@gmail.com>
2022-10-24 18:55:16 +0200

    input element for search term with useState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-
-const API_URL = "https://api.datamuse.com/words?rel_syn=example";
-
-const fetchWords = async () => {
-  try {
-    const response = await fetch(API_URL);
-    if (!response.ok) { // errors from server
-      throw Error(response.statusText);
-    }
-    const data = await response.json();
-    console.log(data)
-    
-  } catch (error) { // errors from network / connection
-    console.log(error.message);
-  }
-};
-
-fetchWords();
-
+import { useState } from 'react';
 
 function App() {
+
+  const API_URL = "https://api.datamuse.com/words?rel_syn=example";
+
+  const fetchWords = async () => {
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) { // errors from server
+        throw Error(response.statusText);
+      }
+      const data = await response.json();
+      console.log(data)
+
+    } catch (error) { // errors from network / connection
+      console.log(error.message);
+    }
+  };
+
+  fetchWords();
+
+  const [changeValue, setChangeValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setChangeValue(event.target.value);
+    console.log(changeValue)
+  };
+
   return (
     <div>
-      Bazinga!
+      <h1>Bazinga!</h1>
+      <input 
+       type="text" 
+       value={changeValue} 
+       onChange={handleInputChange} 
+      />
     </div>
   );
 }
