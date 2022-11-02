a4c1a0b5480ed85025dcb32f0aa4b985b2d55c1d
Clemens Bruesch <clemensbruesch@gmail.com>
2022-11-02 22:40:59 +0100

    results kept in useState; results read out from useState

--- src/components/DisplaySynonyms.js
+++ src/components/DisplaySynonyms.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import SearchTermContext from '../context/SearchTermContext'
 import CallAPIContext from '../context/CallAPIContext'
 
@@ -8,38 +8,32 @@ const DisplaySynonyms = () => {
   const { searchTerm } = useContext(SearchTermContext)
   const { fetchWords } = useContext(CallAPIContext)
 
-  const resultsDiv = document.getElementById('results');
+  const [resultsArray, setResultsArray] = useState([])
 
   let queryString = '/words?rel_syn=' + searchTerm
 
-  const displayResults = (data) => {
-    resultsDiv.innerHTML = '';
-    const resultsList = document.createElement('ul');
-
-    // create 'li' element for each word and insert in 'ul'
-    data.forEach(wordObject => {
-      const {word} = wordObject;
-
-      const wordElement = document.createElement('li');
-      wordElement.innerHTML = word;
-      resultsList.appendChild(wordElement);
-    })
-
-    resultsDiv.appendChild(resultsList);
-  }
-
-  if (searchTerm !== '') {
-    fetchWords(queryString)
-      .then((data) => {
-        displayResults(data)
-      })
-      .catch((error) => console.log(error));
-  }
-
+  useEffect(() => {
+    console.log("useEffect triggered");
+    if (searchTerm !== '') {
+      fetchWords(queryString)
+        .then((data) => {
+          setResultsArray(data)
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [searchTerm, fetchWords, queryString]);
   
   return (
     <div className='DisplaySynonyms'>
-      <div id="results"></div>
+      <ul>
+      { resultsArray.length > 0 && (
+          resultsArray.map((wordObject) => {
+            return (
+              <li key={wordObject.word}>{wordObject.word}</li>
+            );
+          })
+        )}
+      </ul>
     </div>
   )
 }
